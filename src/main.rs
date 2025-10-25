5784db17864d077a609816f3695a54bfd7262d0f
Philipp_EndevourOS <phiipp.senfft@gmail.com>
2025-10-25 17:03:32 +0200

    use mutex to share i2c device

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,58 @@
 #![feature(type_alias_impl_trait)]
 #![feature(impl_trait_in_assoc_type)]
 
+use embassy_embedded_hal::shared_bus::asynch::i2c::{self, I2cDevice};
 use embassy_executor::Spawner;
-use log::{error, info};
+use embassy_sync::{blocking_mutex::raw::NoopRawMutex, mutex::Mutex};
 use embassy_time::{Duration, Timer};
+use esp_hal::{Async, i2c::master};
+use esp_hal::{clock::CpuClock, i2c::master::Config, time::Rate, timer::systimer::SystemTimer};
+use esp_hal_embassy;
+use esp_println::logger::init_logger;
+use log::{error, info};
+use static_cell::StaticCell;
 
+mod sense;
+
+esp_bootloader_esp_idf::esp_app_desc!();
 
 #[esp_hal_embassy::main]
 async fn main(spawner: Spawner) {
-    spawner.spawn(welcome_task()).unwrap();
+    /************* init ******************/
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let p = esp_hal::init(config);
+    init_logger(log::LevelFilter::Debug);
+
     info!("Starting up...");
+
+    let timer0 = SystemTimer::new(p.SYSTIMER);
+    esp_hal_embassy::init(timer0.alarm0);
+    init_logger(log::LevelFilter::Debug);
+
+    static I2C_BUS: StaticCell<Mutex<NoopRawMutex, esp_hal::i2c::master::I2c<Async>>> =
+        StaticCell::new();
+    let i2c = esp_hal::i2c::master::I2c::new(
+        p.I2C0,
+        Config::default().with_frequency(Rate::from_khz(100)),
+    )
+    .unwrap()
+    .with_sda(p.GPIO22)
+    .with_scl(p.GPIO23)
+    .into_async();
+
+    let i2c_bus = &*I2C_BUS.init(Mutex::new(i2c));
+    let i2c_device: i2c::I2cDevice<'static, NoopRawMutex, master::I2c<'static, Async>> =
+        I2cDevice::new(i2c_bus);
+
+    spawner.spawn(welcome_task()).unwrap();
+
+    let _ = sense::start_sense(spawner, i2c_device).await;
 }
 
 #[embassy_executor::task]
 async fn welcome_task() {
-    loop {
-        info!("Hello =)");
-        Timer::after(Duration::from_secs(1)).await;
-    }
+    info!("Hello =)");
+    Timer::after(Duration::from_secs(1)).await;
 }
 
 #[panic_handler]
---
+++ src/sense.rs
@@ -0,0 +1,60 @@
+
+use embassy_embedded_hal::shared_bus::{self, asynch::i2c::{self, I2cDevice}};
+use embassy_executor::Spawner;
+use embassy_sync::{
+    blocking_mutex::raw::{CriticalSectionRawMutex, NoopRawMutex},
+    watch::{DynReceiver, Watch},
+};
+use embassy_time::{Delay, Duration, Timer};
+use ens160_aq::Ens160;
+use ens160_aq::data::{InterruptPinConfig, Measurements};
+use esp_hal::{
+    Async, i2c::master::{Config, I2c}, interrupt::{bind_interrupt, status}, peripherals::{GPIO22, GPIO23, I2C0}, time::Rate
+};
+use log::{error, info};
+
+
+const CO2_CONSUMERS: usize = 1;
+static CO2: Watch<CriticalSectionRawMutex, u16, CO2_CONSUMERS> = Watch::new();
+
+pub fn get_receiver() -> Option<DynReceiver<'static, u16>> {
+    CO2.dyn_receiver()
+}
+
+#[embassy_executor::task]
+pub async fn sense_task(mut ens160: Ens160<I2cDevice<'static, NoopRawMutex, I2c<'static, Async>>, Delay>){
+
+     loop {
+        if let Ok(status) = ens160.get_status().await {
+            if status.new_data_ready() {
+                if let Ok(measurements) = ens160.get_measurements().await{
+                    info!("Measurements: {:#?}", measurements);
+                } else {
+                    info!("Failed to read measurements.");
+                }
+            }
+        } else {
+            info!("Failed to get ENS160 status.");
+        }
+        Timer::after(Duration::from_secs(2)).await;
+    } 
+}
+
+async fn init_ens(i2c_dev: I2cDevice<'static, NoopRawMutex, I2c<'static, Async>>) -> Result<Ens160<I2cDevice<'_, NoopRawMutex, I2c<'static, Async>>, Delay>, &'static str>{
+    let mut ens160 = Ens160::new(i2c_dev, Delay);
+    if let Ok( success) =  ens160.initialize().await {
+        if success {return Ok(ens160)}
+    }
+    let released_dev = ens160.release();
+    let mut ens160_alt = Ens160::new_secondary_address(released_dev, Delay);
+    if let Ok( success) =  ens160_alt.initialize().await {
+        if success {return Ok(ens160_alt)}
+    }
+    ens160_alt.release();
+    return Err("Faild to init ens160");
+}
+
+pub async fn start_sense(spawner: Spawner, i2c_dev: I2cDevice<'static, NoopRawMutex, I2c<'static, Async>>) -> Result<(), &'static str> {
+    let ens160 = init_ens(i2c_dev).await?;
+    spawner.spawn(sense_task(ens160)).map_err(| _ | {"Faild to spawn sense task!"})
+}
